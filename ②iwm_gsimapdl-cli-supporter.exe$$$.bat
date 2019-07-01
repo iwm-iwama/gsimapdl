07c6a122f24fdc6dfab6b10040bc136cc929d43c
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2019-07-01 15:30:28 +0900

    Add files via upload

---
+++ ②iwm_gsimapdl-cli-supporter.exe$$$.bat
@@ -0,0 +1,4 @@
+@echo off
+"D:\__gsimapdl\__GsiMapDlSet\iwm_gsimapdl-cli.exe" -test --gsiserver=https://cyberjapandata.gsi.go.jp/ --zoomlevelmax=18 --wait=5 --dir="D:\__gsimapdl\__GsiMapDlSet\data" "D:\__gsimapdl\__GsiMapDlSet\sample.gtsv"
+echo.
+pause
