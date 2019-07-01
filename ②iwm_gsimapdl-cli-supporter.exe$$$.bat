0104cf7f14643167752d694e225a73d25f4983d7
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2019-07-01 15:33:22 +0900

    Delete ②iwm_gsimapdl-cli-supporter.exe$$$.bat

--- ②iwm_gsimapdl-cli-supporter.exe$$$.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"D:\__gsimapdl\__GsiMapDlSet\iwm_gsimapdl-cli.exe" -test --gsiserver=https://cyberjapandata.gsi.go.jp/ --zoomlevelmax=18 --wait=5 --dir="D:\__gsimapdl\__GsiMapDlSet\data" "D:\__gsimapdl\__GsiMapDlSet\sample.gtsv"
-echo.
-pause
